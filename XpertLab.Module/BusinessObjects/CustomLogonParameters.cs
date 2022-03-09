e1a1a8fd56c175750afa41ba9cde015d8f9af922
someone <ramzymbk@gmail.com>
2022-03-09 09:33:39 +0100

    BlazorCustomConnectionString
    This commit allows you to set custom connection string with MSSQLServer. Only works for Blazor.

--- XpertLab.Module/BusinessObjects/CustomLogonParameters.cs
+++ XpertLab.Module/BusinessObjects/CustomLogonParameters.cs
@@ -13,8 +13,8 @@ using DevExpress.DataAccess.Json;
 using System;
 using System.Collections.Generic;
 using Json.Net;
-using Newtonsoft.Json;
 using System.Text.Json;
+using Microsoft.Extensions.Options;
 
 namespace Chooser.Module.BusinessObjects
 {
@@ -28,6 +28,7 @@ namespace Chooser.Module.BusinessObjects
     [DomainComponent]
     public class CustomLogonParametersForStandardAuthentication : AuthenticationStandardLogonParameters, IDatabaseNameParameter
     {
+
         private string _nameConfigDB = MSSqlServerChangeDatabaseHelper.NameConfigDB.Split(';')[0];
         [ModelDefault("PredefinedValues", MSSqlServerChangeDatabaseHelper.NameConfigDB)]
 
@@ -63,7 +64,7 @@ namespace Chooser.Module.BusinessObjects
             }
         }
     }
-
+    #region
     public class ConfigDB
     {
         public string ConfigName { get; set; }
@@ -77,31 +78,81 @@ namespace Chooser.Module.BusinessObjects
     {
         public List<ConfigDB> ListConfigDB { get; set; }
     }
+    #endregion
+
+    #region 
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class ConnectionStrings
+    {
+        public List<string> CustomConnectionString { get; set; }
+    }
+
+    public class Root2
+    {
+        public ConnectionStrings ConnectionStrings { get; set; }
+    }
+
+
+
+
+    #endregion
+
     public class MSSqlServerChangeDatabaseHelper
     {
-        public const string NameConfigDB = "Home;Work1;Work2";
+
+
+
+
+
+
+
+        public const string NameConfigDB = "XpertClinic;XpertLab";
         public static string PatchConnectionString(string pNameConfig, string connectionString)
         {
 
 
-            var myJsonResponse = File.ReadAllText(@"C:\Users\soufiane\Documents\GitHub\MultiBaseTesting\XpertLab.Module\BusinessObjects\MyLoginData.json");
+            var myJsonResponse = File.ReadAllText(@"C:\Users\Ramzi\Documents\GitHub\MultiBaseTesting\XpertLab.Module\BusinessObjects\MyLoginData.json");
 
             Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
-            string cnn = "";
 
-            foreach (var ItemConfig in myDeserializedClass.ListConfigDB)
+            #region
+
+            var myJsonResponse2 = File.ReadAllText(@"C:\Users\Ramzi\Documents\GitHub\MultiBaseTesting\XpertLab.Blazor.Server\appsettings.json");
+            Root2 myDeserializedClass2 = JsonConvert.DeserializeObject<Root2>(myJsonResponse2);
+
+            string cnn = "";
+            foreach (var myConnectionString in myDeserializedClass2.ConnectionStrings.CustomConnectionString)
             {
-                if (ItemConfig != null)
+                string[] myConnectionBrute = myConnectionString.Split(";");
+
+                ConfigDB myConnection = new ConfigDB { ConfigName = myConnectionBrute[0], ServerName = myConnectionBrute[1], DatabaseName = myConnectionBrute[2], UserName = myConnectionBrute[3], Password = myConnectionBrute[4] };
+
+
+                if (!string.IsNullOrEmpty(myConnection.ConfigName) && myConnection.ConfigName == pNameConfig)
                 {
-                    if (!string.IsNullOrEmpty(ItemConfig.ConfigName) && ItemConfig.ConfigName == pNameConfig)
-                    {
-                        // decryptage pwd
-                        cnn = string.Format("Data Source={0};Initial Catalog ={1}; User ID ={2}; Password ={3}",
-                                ItemConfig.ServerName, ItemConfig.DatabaseName, ItemConfig.UserName, ItemConfig.Password);
-                    }
+                    cnn = string.Format("Data Source={0};Initial Catalog ={1}; User ID ={2}; Password ={3}",
+                                     myConnection.ServerName, myConnection.DatabaseName, myConnection.UserName, myConnection.Password);
+                    return cnn;
+
                 }
             }
 
+            #endregion
+
+            /*
+                        foreach (var ItemConfig in myDeserializedClass.ListConfigDB)
+                        {
+                            if (ItemConfig != null)
+                            {
+                                if (!string.IsNullOrEmpty(ItemConfig.ConfigName) && ItemConfig.ConfigName == pNameConfig)
+                                {
+
+                                       // decryptage pwd
+                                       cnn = string.Format($"{x}; User ID ={ItemConfig.UserName}; Password ={ItemConfig.Password}");
+                                }
+                            }
+                        }
+            */
             return cnn;
         }
     }
